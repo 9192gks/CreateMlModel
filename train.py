91a8e0ad94a2741ae199b318d0547a3d9043c5e4
Gourab Kumar Sahoo <40438474+9192gks@users.noreply.github.com>
2021-03-17 14:45:26 +0530

    Update train.py
    #15 Changed the number of iterations from 2500 to 20000

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ data = tc.SFrame('training.sframe')
 train_data, test_data = data.random_split(0.8)
 #
 # Automatically picks the right model based on your data.
-model = tc.object_detector.create(train_data, feature='image', annotations='annotations', max_iterations=2500)
+model = tc.object_detector.create(train_data, feature='image', annotations='annotations', max_iterations=20000)
 #
 # Save the model for later use in Turi Create
 # Important to save in case something after breaks the script
